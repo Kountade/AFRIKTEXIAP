52de21e1b0c7665e87be52364515b39f21417665
Codons-ensemble <bounamakountagoudiaby@gmail.com>
2026-01-10 14:51:50 +0000

    not fount 1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Important pour Render
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Optionnel : pour éviter les erreurs de chunks
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    strictPort: false,
+    host: true,
+    open: true,
+  }
 })
-
-
-
